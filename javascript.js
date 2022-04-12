e7475334e0c41d3fc8054dcc5f7d4930f98bac24
Sparky-code <dkrizwold@berkeley.edu>
2022-04-12 15:22:21 -0700

    boilerplate

---
+++ javascript.js
@@ -0,0 +1,14 @@
+// creat basic math operators
+//add
+//subtract
+//multiply
+//divide
+
+// create operate() - takes an operator and two numbers, calls a function
+
+//add visuals to html css
+//mock up the visuals
+//add clear button
+
+// link up buttons with JS
+// Make it work!
