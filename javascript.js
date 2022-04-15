6c4cac824929158bc6a8bf7dbc2dca948c2811e6
Sparky-code <dkrizwold@berkeley.edu>
2022-04-14 21:17:21 -0700

    operate function

--- javascript.js
+++ javascript.js
@@ -1,10 +1,45 @@
 // creat basic math operators
+
 //add
+const add = function (a, b) {
+    let result = a + b;
+    return result;
+}
+
 //subtract
+const subtract = function (a, b) {
+    let result = a - b;
+    return result;
+}
+
 //multiply
+const multiply = function (a, b) {
+    let result = a * b;
+    return result;
+}
 //divide
+const divide = function (a, b) {
+    let result = a / b;
+    return result;
+}
+const power = function (a, b) {
+    let result = a ** b;
+    return result
+}
 
 // create operate() - takes an operator and two numbers, calls a function
+function operate(a, b, op) {
+    let opsy = op.name;
+    console.log(opsy)
+    ops = new Object(a, b);
+    ops.add = add;
+    ops.subtract = subtract;
+    ops.multiply = multiply;
+    ops.divide = divide;
+    ops.power = power;
+    let total = ops[`${opsy}`](a, b);
+    return total;
+}
 
 //add visuals to html css
 //mock up the visuals
