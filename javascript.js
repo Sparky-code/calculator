f90ee354bd9c19ac86bb1e43aa593aa46baad7a6
Sparky-code <dkrizwold@berkeley.edu>
2022-04-21 13:17:02 -0700

    leading operator fix, negative exeption still error

--- javascript.js
+++ javascript.js
@@ -3,6 +3,8 @@
 let displayValue = '';
 let currentInput = '';
 let priorInput = '';
+let allowedOperators = /\W$/
+let notAllowedOperators = /^\W/
 let snarkyMessage = 'GOING PLAID';
 const equals = document.getElementById('equals').innerHTML;
 const operators = document.getElementById('operators');
@@ -19,18 +21,17 @@ function clear() {
 
 // INPUT
 
-function inputCheck() {
-
-    if (allowedValues.test(displayValue)) {
-        console.log('YUUUUP!' + displayValue)
-    }
-}
-
 inputOptions.addEventListener('click', function (e) {
+    if ((notAllowedOperators.test(displayValue))) {
+        if (/\-/.test(displayValue)) {
+            displayValue = (displayValue + (e.target.getAttribute('id')));
+            return document.getElementById('screen').innerHTML = displayValue
+        } else
+            clear()
+    }
     if (e.target.getAttribute('id') === 'Clear') {
         clear();
     } else if (displayValue != '') {
-        allowedOperators = /\W$/
         if (allowedOperators.test(e.target.getAttribute('id'))) {
             operate()
         } else
@@ -45,15 +46,15 @@ inputOptions.addEventListener('click', function (e) {
     console.log(displayValue)
 })
 
-// Prohibits Long Strings
+// // Prohibits Long Strings
 
-inputOptions.addEventListener('click', function (e) {
-    if (currentInput.length > 9) {
-        let trimmedValue = currentInput.substring(0, 8)
-        document.getElementById('screen').innerHTML = trimmedValue;
-        console.log(trimmedValue)
-    }
-})
+// inputOptions.addEventListener('click', function (e) {
+//     if (displayValue.length > 9) {
+//         let trimmedValue = displayValue.substring(, -1)
+//         document.getElementById('screen').innerHTML = trimmedValue;
+//         console.log(trimmedValue)
+//     }
+// })
 
 // Equals, Prohibit 1/0, Adds input to display
 
@@ -66,9 +67,10 @@ bottomRow.addEventListener('click', function (e) {
         operate();
         return document.getElementById('screen').innerHTML = displayValue;
     } else
-        currentInput += (e.target.getAttribute('id'));
-    document.getElementById('screen').innerHTML = currentInput
-    console.log(currentInput)
+        console.log(currentInput)
+    displayValue += (e.target.getAttribute('id'));
+    document.getElementById('screen').innerHTML = displayValue
+    console.log(displayValue)
 })
 
 // OPERATION
