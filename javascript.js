6bf1ae8fb17c95bd76b3aaecfe578ec50184d166
Sparky-code <dkrizwold@berkeley.edu>
2022-04-22 16:05:49 -0700

    refactored buttons listeners and button organization

--- javascript.js
+++ javascript.js
@@ -21,32 +21,88 @@ function clear() {
 }
 
 //Input
-
-inputOptions.addEventListener('click', function (e) {
-    if (e.target.getAttribute('id') === 'Clear') {
-        clear();
-        console.log('you pressed clear')
-    } else if (displayValue != '') {
-        if (allowedOperators.test(e.target.getAttribute('id'))) {       // Manages multiple operators
-            if ((/^\-/.test(displayValue))) {
-                console.log(displayValue + ' contains a negative lead')
-                document.getElementById('screen').innerHTML = displayValue.toString()   // Manages negative leads
-            } else if ((notAllowedOperators.test(displayValue))) {
-                console.log(displayValue + ' contains a leading operator')      // Manages leading operators
-                displayValue = displayValue.substring(1, 9);
-            } else
-                operate()
-        } else                                                              // Manages adding to an existing display
-            currentInput = '';
+digits.addEventListener('click', function (e) {
+    if (displayValue != '') {                                                             // Manages adding to an existing display
+        currentInput = '';
         priorInput = '';
         displayValue = ((displayValue) + (e.target.getAttribute('id')));
         document.getElementById('screen').innerHTML = displayValue;
         console.log(displayValue + ' is the increment value');
-    } else                                                                  // Manages New Display
+    } else
         displayValue = currentInput + (e.target.getAttribute('id'));
     document.getElementById('screen').innerHTML = displayValue
     console.log(displayValue + ' is the new value')
 })
+
+operators.addEventListener('click', function (e) {
+    if (e.target.getAttribute('id') == 'Clear') {
+        console.log('clear pressed')
+        return clear()
+    } else if (displayValue != '') {
+        if (/^\-/.test(displayValue)) {
+            console.log(displayValue + ' contains a negative lead')
+            document.getElementById('screen').innerHTML = displayValue.toString()
+        } else if ((notAllowedOperators.test(displayValue))) {
+            console.log(displayValue + ' contains a leading operator')      // Manages leading operators
+            displayValue = displayValue.substring(1, 9);
+        } else if ((allowedOperators.test(displayValue)) && (allowedOperators.test((e.target.getAttribute('id'))))) {
+            console.log('clear for double operator');
+            return document.getElementById('screen').innerHTML = displayValue;
+        } else if (allowedValues.test(displayValue)) {
+            displayValue = displayValue + (e.target.getAttribute('id'));
+            return document.getElementById('screen').innerHTML = displayValue;
+        } else
+            console.log('here')
+        operate()
+    } else
+        currentInput = '';
+    priorInput = '';
+    displayValue = ((displayValue) + (e.target.getAttribute('id')));
+    document.getElementById('screen').innerHTML = displayValue;
+    console.log(displayValue + ' is the increment value');
+})
+
+bottomRow.addEventListener('click', function (e) {
+    if (e.target.getAttribute('id') == 'equals') {
+        console.log('equals')
+        if (allowedOperators.test(displayValue)) {
+            console.log('equals operator end')
+            return displayValue;
+        }
+        console.log('equals operate')
+        operate();
+        document.getElementById('screen').innerHTML = displayValue;
+    }
+})
+// && (allowedOperators.test(displayValue))){
+
+
+// inputOptions.addEventListener('click', function (e) {
+//     if (e.target.getAttribute('id') === 'Clear') {
+//         clear();
+//         console.log('you pressed clear')
+//     } else if (displayValue != '') {
+//         if (allowedOperators.test(e.target.getAttribute('id'))) {       // Manages multiple operators
+//             console.log('issue with parsing after equals')
+//             if ((/^\-/.test(displayValue))) {
+//                 console.log(displayValue + ' contains a negative lead')
+//                 document.getElementById('screen').innerHTML = displayValue.toString()   // Manages negative leads
+//             } else if ((notAllowedOperators.test(displayValue))) {
+//                 console.log(displayValue + ' contains a leading operator')      // Manages leading operators
+//                 displayValue = displayValue.substring(1, 9);
+//             } else
+//                 operate()
+//         } else                                                              // Manages adding to an existing display
+//             currentInput = '';
+//         priorInput = '';
+//         displayValue = ((displayValue) + (e.target.getAttribute('id')));
+//         document.getElementById('screen').innerHTML = displayValue;
+//         console.log(displayValue + ' is the increment value');
+//     } else                                                                  // Manages New Display
+//         displayValue = currentInput + (e.target.getAttribute('id'));
+//     document.getElementById('screen').innerHTML = displayValue
+//     console.log(displayValue + ' is the new value')
+// })
 
 // // Prohibits Long Strings
 
@@ -60,39 +116,25 @@ inputOptions.addEventListener('click', function (e) {
 
 // Equals, Prohibit 1/0, Adds input to display
 
-bottomRow.addEventListener('click', function (e) {
-    if (e.target.getAttribute('id') === 'equals') {
-        if (currentInput == '1/0') {
-            clear()
-            return document.getElementById('screen').innerHTML = snarkyMessage;
-        }
-        operate();
-        return document.getElementById('screen').innerHTML = displayValue;
-    } else
-        displayValue += (e.target.getAttribute('id'));
-    document.getElementById('screen').innerHTML = displayValue
-    console.log(displayValue + ' is the new value with bottom row addition')
-})
+// bottomRow.addEventListener('click', function (e) {
+//     if (e.target.getAttribute('id') === 'equals') {
+//         if (currentInput == '1/0') {
+//             clear()
+//             return document.getElementById('screen').innerHTML = snarkyMessage;
+//         } else
+//             operate();
+//         console.log('second string equals')
+//     } else
+//         displayValue += (e.target.getAttribute('id'));
+//     document.getElementById('screen').innerHTML = displayValue
+//     console.log(displayValue + ' is the new value with bottom row addition')
+// })
 
 // OPERATION
 
 function operate() {
-    if (displayValue < 0) {                     // Manages negative number + eqauls error
-        return displayValue;
-    }
-    if (displayValue.startsWith('-')) {                 // Dealing with negative addition....
-        priorInput = displayValue.substring(1, 5)
-        console.log(priorInput)
-        priorInput = priorInput.split('-');
-        console.log(priorInput)
-        let a = currentInput.concat('-', priorInput[0]);
-        console.log(a);
-        let b = priorInput[1].toString();
-        console.log(priorInput + ' was just split for subtraction');
-        console.log(displayValue = parseFloat(a) - parseFloat(b));
-        num = parseFloat(a) - parseFloat(b);
-        return roundToTwo(num)
-    }
+    // Zeroes();
+    // NegativeNumbers();
     if (displayValue.includes('+')) {
         priorInput = displayValue.split('+', 9);
         let a = priorInput[0].toString();
@@ -135,6 +177,24 @@ function operate() {
         return roundToTwo(num)
     }
 }
+// function Zeroes() {
+//     if (displayValue < 0) {                     // Manages negative number + eqauls error
+//         return displayValue;
+//     }
+// }
+
+// function NegativeNumbers() {
+//     if (displayValue.startsWith('-')) {                 // Dealing with negative addition....
+//         priorInput = displayValue.slice(1).split('-')
+//         console.log(priorInput)
+//         let a = currentInput.concat('-', priorInput[0]);
+//         let b = priorInput[1].toString();
+//         console.log(priorInput + ' was just split for subtraction');
+//         console.log(displayValue = parseFloat(a) - parseFloat(b));
+//         num = parseFloat(a) - parseFloat(b);
+//         return roundToTwo(num)
+//     }
+// }
 
 // ROUNDING
 
